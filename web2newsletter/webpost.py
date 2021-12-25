1041a3d10e847677127c0206e9afa11221a97745
navanchauhan <navanchauhan@gmail.com>
2021-12-25 16:29:04 -0700

    code to fetch and extract text

---
+++ web2newsletter/webpost.py
@@ -0,0 +1,83 @@
+import logging
+import requests
+import feedparser
+import time
+import datetime
+import re
+import html2text
+
+
+from usp.tree import sitemap_tree_for_homepage
+from urllib.parse import urlparse
+from tqdm import tqdm
+from readability import Document
+
+logging.getLogger("usp.fetch_parse").setLevel(logging.FATAL)
+logging.getLogger("usp.helpers").setLevel(logging.FATAL)
+logging.getLogger("usp.tree").setLevel(logging.FATAL)
+
+class WebPost:
+	def __init__(self, title, url, date, content, content_type: str = "plain"):
+		self.title = title
+		self.url = url
+		self.date = date
+		self.content = content
+		self.content_type = content_type
+	def __str__(self):
+		return f"Post: {self.title}\nPublished on {self.date} at {self.url}"
+
+class WebPostSource:
+	def __init__(self, title):
+		self.title = title
+		self.web_posts = []
+
+	def add_post(self, post: WebPost):
+		self.web_posts.append(post)
+
+	def get_sorted_posts(self):
+		return sorted(self.web_posts, key = lambda x: time.mktime(x.date), reverse=True)
+
+def get_content(link,config,website: WebPostSource):
+	res = requests.head(link)
+	"""TODO
+	Custom Exceptions
+	"""
+	content_type = res.headers["Content-Type"]
+	if "application/rss+xml" in content_type:
+		feed = feedparser.parse(link)
+		t1 = time.time()
+		for entry in feed["entries"]:
+			t2 = time.mktime(entry.updated_parsed)
+			if datetime.timedelta(seconds=t1-t2).days > int(config["FEEDS"]["LastNDays"]):
+				continue
+			else:
+				post = WebPost(entry.title,entry.link,entry.updated_parsed,entry.summary)
+				website.add_post(post)
+	else:
+		t1 = time.time()
+		tree = sitemap_tree_for_homepage(link)
+		posts_to_search = {}
+		for page in tqdm(tree.all_pages()):
+			if datetime.timedelta(seconds=t1-page.last_modified.timestamp()).days > int(config["FEEDS"]["LastNDays"]):
+				continue
+			else:
+				if any(ignore in page.url for ignore in config["MISC_SITEMAP"]["IgnoreIfInURL"].split()):
+					continue
+				else:
+					a = urlparse(page.url)
+					if f"{a.scheme}://{a.netloc}/"==page.url:
+						continue
+					else:
+						posts_to_search[page.url] = page.last_modified
+		if posts_to_search != {}:
+			h = html2text.HTML2Text()
+			h.ignore_links = True
+			for url in posts_to_search:
+				res = requests.get(url)
+				doc = Document(res.text)
+				doc_text = html2text.html2text(doc.summary())
+				post = WebPost(doc.title(),url,posts_to_search[url].timetuple(),doc_text,"markdown")
+				website.add_post(post)
+
+
+
